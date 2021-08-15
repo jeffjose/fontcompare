03062e9ba391fb8155f7707c022ec030f8fcbe70
Jeffrey Jose <jeffjosejeff@gmail.com>
2021-08-14 22:38:16 -0700

    display chars at baseline.

--- src/components/FontDisplay.svelte
+++ src/components/FontDisplay.svelte
@@ -6,22 +6,21 @@
 
 <div class="displaygroup">
   {#each [...$fonts] as font}
-    <div class="display {utils.getFontClassName(font)}">Aa</div>
+    <div class="display {utils.getFontClassName(font)}">A</div>
   {/each}
 </div>
 
 <style scoped>
   .displaygroup {
     height: 100vh;
-    background-color: #ff00000a;
     display: flex;
     flex-direction: row;
-    justify-content: space-around;
+    justify-content: space-evenly;
   }
 
   .display {
-    height: 200px;
-    width: 100px;
-    font-size: 100pt;
+    font-size: 150pt;
+    align-self: baseline;
+    background-color: white;
   }
 </style>
