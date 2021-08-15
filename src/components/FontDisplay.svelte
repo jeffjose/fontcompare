52837b4d7e217551c062cca9814efe9f2eb6aa4e
Jeffrey Jose <jeffjosejeff@gmail.com>
2021-08-15 14:54:17 -0700

    overline, underline

--- src/components/FontDisplay.svelte
+++ src/components/FontDisplay.svelte
@@ -2,33 +2,67 @@
   import * as utils from "../utils.js";
 
   import { fonts } from "../stores.js";
+
+  let glyphs = ["A", "C", "I", "O", "g", "M", "Mg", "q", "bdpq"];
 </script>
 
-<div class="displaygroup">
+<div>
+  {#each glyphs as glyph}
+    <div class="glyphrow">
+      {#each [...$fonts] as font}
+        <span class="glyph {utils.getFontClassName(font)}">
+          {glyph}
+        </span>
+      {/each}
+    </div>
+  {/each}
+</div>
+
+<!--
+
+<div class="wrapper">
   {#each [...$fonts] as font}
-    <div class="display {utils.getFontClassName(font)}">
-      <div>A</div>
-      <div>B</div>
-      <div>C</div>
+    <div class="fontcolumn">
+      <div class="glyph {utils.getFontClassName(font)}">
+        {#each glyphs as glyph}
+          {glyph}<br />
+        {/each}
+      </div>
     </div>
   {/each}
 </div>
 
 <style scoped>
-  .displaygroup {
-    height: 100vh;
+  .fontcolumn {
+    display: flex;
+    align-items: last baseline;
+  }
+
+  .glyph {
+    font-size: 150pt;
+    text-decoration: underline;
+  }
+
+  .wrapper {
     display: flex;
     flex-direction: row;
+  }
+</style>
+
+-->
+<style scoped>
+  .glyphrow {
+    display: flex;
+    align-items: baseline;
     justify-content: space-evenly;
   }
 
-  .display {
-    display: flex;
-    flex-direction: column;
-    align-self: baseline;
-  }
-  .display * {
-    font-size: 200pt;
-    align-self: baseline;
+  .glyph {
+    font-size: 150pt;
+    text-decoration: underline;
+    /*text-decoration: line-through overline underline; */
+    text-decoration-color: red;
+    text-decoration-thickness: 1px;
+    color: #ff000070;
   }
 </style>
